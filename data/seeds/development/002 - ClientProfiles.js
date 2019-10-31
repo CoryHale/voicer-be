0dcb621265f9887ac16f5526f107d94fa507b715
Ken Ruf <ruf.kenichi@gmail.com>
2019-10-30 21:56:57 -0500

    added cascade onDelete and switched from truncate to del

--- data/seeds/development/002 - ClientProfiles.js
+++ data/seeds/development/002 - ClientProfiles.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('clientProfiles').truncate()
+  return knex('clientProfiles').del()
     .then(function () {
       // Inserts seed entries
       return knex('clientProfiles').insert([
--- data/seeds/development/003 - TalentProfiles.js
+++ data/seeds/development/003 - TalentProfiles.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('talentProfiles').truncate()
+  return knex('talentProfiles').del()
     .then(function () {
       // Inserts seed entries
       return knex('talentProfiles').insert([
