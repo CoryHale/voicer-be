538312757516d60511b751b3d2404ad3acadc46d
Jeremy Morales <jeremy.morales@gmail.com>
2019-11-04 20:54:13 -0800

    fixed server port conflict I created

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 const server = require('./server');
 
-const PORT = process.env.PORT || 5432;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => console.log(`Hello World! Port: ${PORT}`));
