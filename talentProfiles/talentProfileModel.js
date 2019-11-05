d41779ef4f9b20280e21f0a8ef19e200f1c4ed3b
Jeremy Morales <jeremy.morales@gmail.com>
2019-11-04 20:55:57 -0800

    added basic language endpoints

--- talentProfiles/talentProfileModel.js
+++ talentProfiles/talentProfileModel.js
@@ -1,77 +1,106 @@
 const db = require('../data/dbConfig.js');
 
-const addTalentProfile = async (profile) => {
-    return await db('talentProfiles').insert(profile)
-}
+const addTalentProfile = async profile => {
+  return await db('talentProfiles').insert(profile);
+};
 
 const updateTalentProfile = async (talentId, profileData) => {
-    try {
-        const checkForProfile = await db('talentProfiles').where({ talentId });
-        if (!(checkForProfile)) return null;
-        
-        const updatedProfile = await db('talentProfiles').where({ talentId }).update(profileData);
-        if (!(updatedProfile)) return null;
+  try {
+    const checkForProfile = await db('talentProfiles').where({ talentId });
+    if (!checkForProfile) return null;
 
-        const selectUpdatedProfile = await db('talentProfiles').where({ talentId });
-        if (!(selectUpdatedProfile)) return null;
+    const updatedProfile = await db('talentProfiles')
+      .where({ talentId })
+      .update(profileData);
+    if (!updatedProfile) return null;
 
-        return selectUpdatedProfile;
-    }
-    catch (err) {
-        return err.message;
-    }
-}
+    const selectUpdatedProfile = await db('talentProfiles').where({ talentId });
+    if (!selectUpdatedProfile) return null;
 
-const deleteTalentProfile = async (talentId) => {
-    try{
-        
-        const selectedProfile = await db('talentProfiles').where({ talentId }).first();
-        if (!(selectedProfile)) return null;
+    return selectUpdatedProfile;
+  } catch (err) {
+    return err.message;
+  }
+};
 
-        const deletedProfile = await db('talentProfiles').where({ talentId }).del();
-        if (!(deletedProfile)) return null;
-        
-        return selectedProfile;
+const deleteTalentProfile = async talentId => {
+  try {
+    const selectedProfile = await db('talentProfiles')
+      .where({ talentId })
+      .first();
+    if (!selectedProfile) return null;
 
-    }
-    catch (err) {
-        return err.message;
-    }
-}
+    const deletedProfile = await db('talentProfiles')
+      .where({ talentId })
+      .del();
+    if (!deletedProfile) return null;
+
+    return selectedProfile;
+  } catch (err) {
+    return err.message;
+  }
+};
 
 const getTalentProfiles = _ => {
-    return db('talentProfiles');
+  return db('talentProfiles');
+};
+
+const getTalentProfileById = async talentId => {
+  try {
+    const selectedProfile = await db('talentProfiles')
+      .where({ talentId })
+      .first();
+    return selectedProfile ? selectedProfile : null;
+  } catch {
+    return null;
+  }
+};
+
+const getTalentProfileByUserId = async userId => {
+  try {
+    const selectedProfile = await db('users as usr')
+      .join('talentProfiles as talPro', 'usr.userId', '=', 'talPro.userId')
+      .select(
+        'usr.userId',
+        'usr.username',
+        'usr.userType',
+        'usr.email',
+        'usr.firstName',
+        'usr.lastName',
+        'talPro.clientId',
+        'talPro.companyName'
+      );
+
+    return selectedProfile ? selectedProfile : null;
+  } catch {
+    return null;
+  }
+};
+
+const getLanguages = () => {
+  return db('languages');
+};
+
+async function addLanguage(language) {
+  return await db('languages').insert(language);
 }
 
-const getTalentProfileById = async (talentId) => {
-  try{
-      const selectedProfile = await db('talentProfiles').where({ talentId }).first();
-      return (selectedProfile) ? selectedProfile : null;
+const updateLanguage = async (languageId, languageUpdate) => {
+  try {
+    const langCheck = await db('languages').where({ languageId });
+  } catch (err) {
+    return err;
   }
-  catch {
-      return null;
-  }
-}
-
-const getTalentProfileByUserId = async (userId) => {
-    try{
-        const selectedProfile = await
-        db('users as usr')
-        .join('talentProfiles as talPro', 'usr.userId', '=', 'talPro.userId')
-        .select('usr.userId', 'usr.username', 'usr.userType', 'usr.email', 'usr.firstName', 'usr.lastName', 'talPro.clientId', 'talPro.companyName')
-
-        return (selectedProfile) ? selectedProfile : null;
-    }
-    catch {
-        return null;
-    }
-  }
+};
 
 module.exports = {
-    addTalentProfile,
-    updateTalentProfile,
-    deleteTalentProfile,
-    getTalentProfiles,
-    getTalentProfileById,
-    getTalentProfileByUserId
-}
+  addTalentProfile,
+  updateTalentProfile,
+  deleteTalentProfile,
+  getTalentProfiles,
+  getTalentProfileById,
+  getTalentProfileByUserId,
+  getLanguages,
+  addLanguage,
+  updateLanguage
+};
--- talentProfiles/talentProfileRouter.js
+++ talentProfiles/talentProfileRouter.js
@@ -8,50 +8,54 @@ const TalentProfiles = require('./talentProfileModel.js');
 // FIXME: Add auth for all endpoints and verify user is the owner
 
 // Insert talent profile into db
-router.post('/', async ( req, res ) => {
-    const profileData = req.body;
-    
-    try {
-        await TalentProfiles.addTalentProfile(profileData);
-        res.status(201).json({message: "Added talent profile."})
-    }
-    catch(error) {
-        res.status(500).json({message: "Could not add talent profile", error: error})
-    }
-})
+router.post('/', async (req, res) => {
+  const profileData = req.body;
+
+  try {
+    await TalentProfiles.addTalentProfile(profileData);
+    res.status(201).json({ message: 'Added talent profile.' });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Could not add talent profile', error: error });
+  }
+});
 
 // Update existing talent profile
-router.put('/:id', async ( req, res ) => {
-    const profileData = req.body;
-    const { id } = req.params;
+router.put('/profile/:id', async (req, res) => {
+  const profileData = req.body;
+  const { id } = req.params;
 
-    try {
-        const updatedData = await TalentProfiles.updateTalentProfile(id, profileData);
-        res.status(201).json(updatedData)
-    }
-    catch(error) {
-        res.status(500).json({message: "Talent profile could not be updated", error: error})
-    }
-})
+  try {
+    const updatedData = await TalentProfiles.updateTalentProfile(
+      id,
+      profileData
+    );
+    res.status(201).json(updatedData);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Talent profile could not be updated', error: error });
+  }
+});
 
 // Remove talent profile
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
-    
-    try {
-      const deletedProfile = await TalentProfiles.deleteTalentProfile(id);
-  
-      if (deletedProfile) {
-        res.status(201).json(deletedProfile);
-      }
-      else {
-        res.status(404).json({ message: 'Could not delete talent profile.' });
-      }
+router.delete('/profile/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedProfile = await TalentProfiles.deleteTalentProfile(id);
+
+    if (deletedProfile) {
+      res.status(201).json(deletedProfile);
+    } else {
+      res.status(404).json({ message: 'Could not delete talent profile.' });
     }
-    
-    catch (error) {
-      res.status(500).json({ message: 'Could not delete talent profile.', error: error });
-    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Could not delete talent profile.', error: error });
+  }
 });
 
 // Get all talent profiles
@@ -59,27 +63,53 @@ router.get('/', async (req, res) => {
   try {
     const talentProfiles = await TalentProfiles.getTalentProfiles();
     res.json(talentProfiles);
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json({ message: 'Could not find talent profiles' });
   }
 });
 
 // Get talent profile by user id
-router.get('/:id', async (req, res) => {
+router.get('/profile/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
-    const selectedTalentProfile = await TalentProfiles.getTalentProfileByUserId(id);
+    const selectedTalentProfile = await TalentProfiles.getTalentProfileByUserId(
+      id
+    );
 
     if (selectedTalentProfile) {
       res.json(selectedTalentProfile);
     } else {
-      res.status(404).json({ message: 'Could not find talent profile.' })
+      res.status(404).json({ message: 'Could not find talent profile.' });
     }
   } catch (err) {
     res.status(500).json({ message: 'Could not find talent profile.' });
   }
 });
+
+// Add allowed languages
+router.post('/languages', async (req, res) => {
+  try {
+    const language = await TalentProfiles.addLanguage(req.body);
+    res.status(201).json(language);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'There was an error adding the language' });
+  }
+});
+
+router.get('/languages', async (req, res) => {
+  try {
+    const languages = await TalentProfiles.getLanguages();
+    res.status(200).json(languages);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: 'There was an error retrieving languages' });
+  }
+});
+
+router.post('/');
 
 module.exports = router;
