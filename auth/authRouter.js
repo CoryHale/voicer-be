cebc5c3e28aa3510c3041b4ac8a59e3a39e913f4
Jeremy Morales <jeremy.morales@gmail.com>
2019-11-06 15:21:08 -0800

    add userId and userType to login response

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -4,18 +4,17 @@ const jwt = require('jsonwebtoken');
 
 const Users = require('../users/userModel');
 
-router.post('/register', async ( req, res ) => {
+router.post('/register', async (req, res) => {
   const user = req.body;
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
   try {
-      await Users.addUser(user);
-      res.status(201).json({message: "New User Registered", user: user})
+    await Users.addUser(user);
+    res.status(201).json({ message: 'New User Registered', user: user });
+  } catch (error) {
+    res.status(500).json({ message: 'Could Not Register User', error: error });
   }
-  catch(error) {
-      res.status(500).json({message: "Could Not Register User", error: error})
-  }
-})
+});
 
 router.post('/login', async (req, res) => {
   let { username, password } = req.body;
@@ -25,39 +24,42 @@ router.post('/login', async (req, res) => {
 
     if (user && bcrypt.compareSync(password, user.password)) {
       const token = genToken(user);
+      const { userId, userType } = user;
       res.status(200).json({
         message: `Welcome ${user.username}!`,
-        token: token
+        token,
+        userId,
+        userType
       });
     } else {
       res.status(401).json({ message: 'Invalid Credentials' });
     }
-  } catch(error) {
-    res.status(500).json({message: "Error logging in user", error: error})
+  } catch (error) {
+    res.status(500).json({ message: 'Error logging in user', error: error });
   }
 });
 
 const genToken = user => {
   const payload = {
-    subject: "user",
+    subject: 'user',
     userId: user.userId,
     username: user.username,
     userType: user.userType
-  }
+  };
 
-  console.log(payload)
+  console.log(payload);
 
-  const secret = process.env.secret;
+  const secret = process.env.JWT_SECRET;
 
-  console.log(secret)
+  console.log(secret);
 
   const options = {
     expiresIn: '1h'
-  }
+  };
 
-  console.log(options)
+  console.log(options);
 
-  return jwt.sign(payload, secret, options)
-}
+  return jwt.sign(payload, secret, options);
+};
 
 module.exports = router;
