186536d69dc9428488fad0aad8d0736be910515d
Cesare Ferrari <cesare@maryandferrari.com>
2019-11-19 23:01:36 -0500

    Add status to job seeds

--- data/seeds/development/004 - Jobs.js
+++ data/seeds/development/004 - Jobs.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('jobs').insert([
-        {clientId: 1, jobTitle: 'Job One Title', jobDescription: 'Job One Description', createdDate: new Date().toISOString().slice(0, 19).replace('T', ' '), initialPrice: 300},
-        {clientId: 1, jobTitle: 'Job Two Title', jobDescription: 'Job Two Description', createdDate: new Date().toISOString().slice(0, 19).replace('T', ' '), initialPrice: 500},
-        {clientId: 2, jobTitle: 'Job Three Title', jobDescription: 'Job Three Description', createdDate: new Date().toISOString().slice(0, 19).replace('T', ' '), initialPrice: 700},
+        {clientId: 1, jobTitle: 'Job One Title', jobDescription: 'Job One Description', createdDate: new Date().toISOString().slice(0, 19).replace('T', ' '), initialPrice: 300, status: 'Status1'},
+        {clientId: 1, jobTitle: 'Job Two Title', jobDescription: 'Job Two Description', createdDate: new Date().toISOString().slice(0, 19).replace('T', ' '), initialPrice: 500, status: 'Status1'},
+        {clientId: 2, jobTitle: 'Job Three Title', jobDescription: 'Job Three Description', createdDate: new Date().toISOString().slice(0, 19).replace('T', ' '), initialPrice: 700, status: 'Status1'},
       ]);
     });
 };
