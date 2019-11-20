197dd927c12e65288974bdbcc3fecdb83b47a181
Jacob Ryans <foemaster@live.com>
2019-11-19 21:55:53 -0600

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -33,12 +33,7 @@ module.exports = {
 
   staging: {
     client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_TESTING
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
