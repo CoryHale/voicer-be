288b9ad42c4e8ec4542ac208008d0591e6ba250e
Ken Ruf <ruf.kenichi@gmail.com>
2019-12-10 22:57:10 -0600

    added accountBalance

--- data/migrations/20191210223843_addLoyaltyLevel.js
+++ data/migrations/20191210223843_addLoyaltyLevel.js
@@ -1,8 +1,9 @@
 
 exports.up = function(knex) {
     return knex.schema.table('users', tbl => {
-        tbl.string('loyaltyLevel').defaultTo("Bronze");
+        tbl.integer('loyaltyLevel').defaultTo(1);
         tbl.integer('completedJobs').defaultTo(0);
+        tbl.decimal('accountBalance', 8, 2).defaultTo(0);
     });
 };
 
@@ -10,5 +11,6 @@ exports.down = function(knex) {
     return knex.schema.table('users', tbl => {
         tbl.dropColumn('loyaltyLevel');
         tbl.dropColumn('completedJobs');
+        tbl.dropColumn('accountBalance');
     });
 };
