a8e8b5cf214ed421ca2de6f03fa18c3b9584ae7a
renegade27 <foemaster@live.com>
2019-12-11 22:09:35 -0600

    Commit test cronjob to branch. Removed a random console.log aswell. Dates now being generated without minutes/hours

---
+++ cronJob.js
@@ -0,0 +1,44 @@
+const cron = require('node-cron');
+const Users = require('./users/userModel.js');
+const JobOffers = require('./jobOffers/jobOfferModel.js');
+const TalentProfiles = require('./talentProfileModel.js');
+const ClientProfiles = require('./clientProfileModel.js');
+
+const schedule = () =>
+cron.schedule("* 0 1 * *", async function() {
+        const users = await Users.getUsers()
+        users.forEach(user => {
+            const profile = user.userType === "Talent"
+             ? TalentProfiles.getTalentProfileByUserId(user.userId) 
+             : ClientProfiles.getClientProfileByUserId(user.userId)
+            let offers = user.userType === "Talent"
+             ? JobOffers.getJobOffersByTalentId(profile.talentId)
+             : JobOffers.getJobOffersByClientId(profile.clientId)
+            var cutoffDate = new Date().toISOString().slice(0, 10).replace('T', ' ');
+            cutoffDate.setMonth(d.getMonth() - 6);
+            testDate = new Date().toISOString().slice(0, 10).replace('T', ' ');
+            let completedOffers = offers.filter(offer => {
+                // let completedDate = new Date(offer.completedDate)
+                if (offer.status.toLowerCase() === "Accepted"
+                && testDate >= cutoffDate) {
+                    return offer
+                }
+            })
+            let loyalty = 1
+            if (10 <= completedOffers.length <= 24) {
+                loyalty = 2
+            }
+            if (completedOffers.length >= 25) {
+                loyalty = 3
+            }
+            if (completedOffers.length !== user.completedJobs) {
+                Users.updateUser(user.userId, {
+                    completedJobs: completedOffers.length,
+                    loyaltyLevel: loyalty
+                })
+            }
+        })
+})
+
+
+export default schedule
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
+const schedule = require('./cronJob.js');
 
 const server = require('./server');
 
 const PORT = process.env.PORT || 4000;
+
+schedule()
 
 server.listen(PORT, () => console.log(`Hello World! Port: ${PORT}`));
