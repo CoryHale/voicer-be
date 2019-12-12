eb205ee4a0ba70a858c393c8c704e9201855ed57
Ken Ruf <ruf.kenichi@gmail.com>
2019-12-11 21:33:09 -0600

    added completedDate to jobOffers

--- data/migrations/20191210223843_addLoyaltyLevel.js
+++ data/migrations/20191210223843_addLoyaltyLevel.js
@@ -1,16 +1,24 @@
 
 exports.up = function(knex) {
-    return knex.schema.table('users', tbl => {
-        tbl.integer('loyaltyLevel').defaultTo(1);
-        tbl.integer('completedJobs').defaultTo(0);
-        tbl.decimal('accountBalance', 8, 2).defaultTo(0);
-    });
+    return knex.schema
+        .table('users', tbl => {
+            tbl.integer('loyaltyLevel').defaultTo(1);
+            tbl.integer('completedJobs').defaultTo(0);
+            tbl.decimal('accountBalance', 8, 2).defaultTo(0);
+        })
+        .table('jobOffers', tbl => {
+            tbl.date('completedDate');
+        });
 };
 
 exports.down = function(knex) {
-    return knex.schema.table('users', tbl => {
+    return knex.schema
+    .table('users', tbl => {
         tbl.dropColumn('loyaltyLevel');
         tbl.dropColumn('completedJobs');
         tbl.dropColumn('accountBalance');
+    })
+    .table('jobOffers', tbl => {
+        tbl.dropColumn('completedDate');
     });
 };
