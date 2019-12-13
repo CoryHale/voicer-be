b9a905a144cf45e0c3c79bdacb6049c59e196fad
Ken Ruf <ruf.kenichi@gmail.com>
2019-12-12 22:01:32 -0600

    testing profile and offers

--- cronJob.js
+++ cronJob.js
@@ -6,23 +6,24 @@ const ClientProfiles = require('./clientProfiles/clientProfileModel.js');
 
 const schedule = () => {
     cron.schedule("0 * * * * *", async () => {
-        console.log("Schedule");
         const users = await Users.getUsers();
-        console.log(users)
         users.forEach(async user => {
             const profile = user.userType === "Talent"
                 ? TalentProfiles.getTalentProfileByUserId(user.userId) 
                 : ClientProfiles.getClientProfileByUserId(user.userId)
-            let offers = user.userType === "Talent"
+            console.log(profile)
+            const offers = user.userType === "Talent"
                 ? JobOffers.getJobOffersByTalentId(profile.talentId)
                 : JobOffers.getJobOffersByClientId(profile.clientId)
+            console.log(offers)
             var cutoffDate = new Date()
             //var cutoffDate = new Date().toISOString().slice(0, 10).replace('T', ' ');
             cutoffDate.setMonth(cutoffDate.getMonth() - 6);
             //var testDate = new Date().toISOString().slice(0, 10).replace('T', ' ');
+            var testDate= new Date()
             let completedOffers = offers.filter(offer => {
-                // let completedDate = new Date(offer.completedDate)
-                ((offer.status.toLowerCase() === "accepted") && (offer.completedDate >= cutoffDate))
+                //let completedDate = new Date(offer.completedDate)
+                ((offer.status.toLowerCase() === "accepted") && (testDate >= cutoffDate))
             })
             let loyalty = 1
             if (10 <= completedOffers.length <= 24) {
