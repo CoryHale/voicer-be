1d3afc2123d800be4fccf70be3d3f6cc23d7f08d
Jeremy Morales <jeremy.morales@gmail.com>
2019-12-12 20:31:42 -0800

    add route

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
-const userRouter = require("./users/userRouter.js");
-const authRouter = require("./auth/authRouter");
-const jobRouter = require("./jobs/jobRouter");
-const jobOfferRouter = require("./jobOffers/jobOfferRouter");
-const ClientProfileRouter = require("./clientProfiles/clientProfileRouter");
-const talentProfileRouter = require("./talentProfiles/talentProfileRouter");
-const restricted = require("./auth/authenticate-middleware");
+const userRouter = require('./users/userRouter.js');
+const authRouter = require('./auth/authRouter');
+const jobRouter = require('./jobs/jobRouter');
+const jobOfferRouter = require('./jobOffers/jobOfferRouter');
+const ClientProfileRouter = require('./clientProfiles/clientProfileRouter');
+const talentProfileRouter = require('./talentProfiles/talentProfileRouter');
+const restricted = require('./auth/authenticate-middleware');
+const sign_s3 = require('./uploads/signS3');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
+server.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
-server.use("/api/users", restricted, userRouter);
-server.use("/api/auth", authRouter);
-server.use("/api/jobs", restricted, jobRouter);
-server.use("/api/jobs", restricted, jobOfferRouter);
-server.use("/api/clients", restricted, ClientProfileRouter);
-server.use("/api/talents", restricted, talentProfileRouter);
+server.use('/api/users', restricted, userRouter);
+server.use('/api/auth', authRouter);
+server.use('/api/jobs', restricted, jobRouter);
+server.use('/api/jobs', restricted, jobOfferRouter);
+server.use('/api/clients', restricted, ClientProfileRouter);
+server.use('/api/talents', restricted, talentProfileRouter);
+server.use('/api/uploads', restricted, sign_s3.sign_s3);
 
-server.get("/", (req, res) => {
-  res.send("<h1>Backend API for Voicer</h1>");
+server.get('/', (req, res) => {
+  res.send('<h1>Backend API for Voicer</h1>');
 });
-
 
 module.exports = server;
