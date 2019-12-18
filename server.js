24ae173c516f26d0fa67d86065f146b3335ef03d
Jeremy Morales <jeremy.morales@gmail.com>
2019-12-17 21:13:07 -0800

    reverting cors testing

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ const ClientProfileRouter = require('./clientProfiles/clientProfileRouter');
 const talentProfileRouter = require('./talentProfiles/talentProfileRouter');
 const restricted = require('./auth/authenticate-middleware');
 const sign_s3 = require('./uploads/signS3');
-const voiceSampleRouter = require('./talentVoiceSamples/voiceSampleRouter');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors({ origin: 'https://voicer-app.com', credentials: true }));
+// server.use(cors({ origin: 'https://voicer-app.com', credentials: true }));
+server.use(cors());
 
 server.use('/api/users', restricted, userRouter);
 server.use('/api/auth', authRouter);
@@ -25,7 +25,6 @@ server.use('/api/jobs', restricted, jobOfferRouter);
 server.use('/api/clients', restricted, ClientProfileRouter);
 server.use('/api/talents', restricted, talentProfileRouter);
 server.use('/api/uploads', restricted, sign_s3.sign_s3);
-server.use('/api/talents/voice-samples', cors(), restricted, voiceSampleRouter);
 
 server.get('/', (req, res) => {
   res.send('<h1>Backend API for Voicer</h1>');
