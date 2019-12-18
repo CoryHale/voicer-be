68e34cbb641536dd0f5268930c059f9d948b2842
Jeremy Morales <jeremy.morales@gmail.com>
2019-12-17 20:46:10 -0800

    testing cors fix

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-
 const userRouter = require('./users/userRouter.js');
 const authRouter = require('./auth/authRouter');
 const jobRouter = require('./jobs/jobRouter');
@@ -11,14 +10,13 @@ const ClientProfileRouter = require('./clientProfiles/clientProfileRouter');
 const talentProfileRouter = require('./talentProfiles/talentProfileRouter');
 const restricted = require('./auth/authenticate-middleware');
 const sign_s3 = require('./uploads/signS3');
-const voiceSampleRouter = require("./talentVoiceSamples/voiceSampleRouter");
+const voiceSampleRouter = require('./talentVoiceSamples/voiceSampleRouter');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
-
+server.use(cors({ origin: 'https://voicer-app.com', credentials: true }));
 
 server.use('/api/users', restricted, userRouter);
 server.use('/api/auth', authRouter);
@@ -27,10 +25,10 @@ server.use('/api/jobs', restricted, jobOfferRouter);
 server.use('/api/clients', restricted, ClientProfileRouter);
 server.use('/api/talents', restricted, talentProfileRouter);
 server.use('/api/uploads', restricted, sign_s3.sign_s3);
-server.use("/api/talents/voice-samples", restricted, voiceSampleRouter);
+server.use('/api/talents/voice-samples', restricted, voiceSampleRouter);
 
 server.get('/', (req, res) => {
-    res.send('<h1>Backend API for Voicer</h1>');
+  res.send('<h1>Backend API for Voicer</h1>');
 });
 
 module.exports = server;
